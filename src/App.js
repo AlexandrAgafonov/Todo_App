9b86da19d2f1d14ad382381a955719e264367f9c
Alexandr <7548603@ukr.net>
2023-07-25 18:04:42 +0300

    added Component Actions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import TodoForm from './components/Todos/TodoForm'
 import TodoList from './components/Todos/TodoList'
+import TodosActions from './components/Todos/TodosActions'
 import './App.css'
 
 function App() {
@@ -26,11 +27,24 @@ function App() {
       ? {...todo,isCompleted: !todo.isCompleted}: {...todo}
     ))
   }
+  const resetTodoHandler = ()=>{
+    setTodos ([])
+  }
+
+  const deleteCompletedTodosHandler = ()=>{
+    setTodos (todos.filter((todo) =>
+      !todo.isCompleted
+    ))
+  }
+
+  const completedTodosCount = todos.filter ((todo) => todo.isCompleted).length
  
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
+      {!!todos.length &&(<TodosActions completedTodosExist={!!completedTodosCount} resetTodos={resetTodoHandler} deleteCompletedTodos={deleteCompletedTodosHandler} />)}
+      
       <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodo={toggleTodoHandler}/>
     </div>
   )
